5dd73b360401e092818ed36e13704e1e94d2bbe9
Alfred Peter <alpeter91@gmail.com>
2023-06-16 14:48:51 +0300

    Add clear-all function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import taskTemplate from './taskTemplate.js';
 import TaskList from './taskList.js';
 import Task from './task.js';
 import UtilityFunctions from './utilityFunctions.js';
+import  clearCompleted from './clear-completed.js'
 import './style.css';
 
 const container = document.getElementById('toDo');
@@ -33,21 +34,23 @@ form.addEventListener('submit', (e) => {
 });
 
 // Checkbox functionality
-const checkedArray = [];
-const checkboxes = container.querySelectorAll('input[type="checkbox"]');
-checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener('change', () => {
+container.addEventListener('change', (e) => {
+  if (e.target.classList.contains('status')) {
+    const checkbox = e.target
     const label = checkbox.nextElementSibling;
     const taskNum = checkbox.dataset.index;
     const idx = parseInt(taskNum, 10);
-    checkedArray.push(idx);
     if (checkbox.checked) {
       label.style.textDecoration = 'line-through';
+      taskArray[idx].completed = true;
+      console.log(idx, taskArray[idx].index, taskArray[idx].completed);
     } else {
       label.style.textDecoration = 'none';
+      taskArray[idx].completed = false;
+      console.log(idx, taskArray[idx].index, taskArray[idx].completed);
     }
-  });
-});
+  }
+})
 
 // Editing and deleting tasks
 container.addEventListener('click', (e) => {
@@ -86,4 +89,13 @@ container.addEventListener('click', (e) => {
       }
     });
   }
+});
+
+// Clear completed tasks
+clear.addEventListener('click', (e) => {
+  e.preventDefault();
+  let moddedArray = clearCompleted(taskArray);
+  moddedArray = UtilityFunctions.addIndex(moddedArray)
+  UtilityFunctions.setStorage(moddedArray)
+  UtilityFunctions.showTasks(list, taskTemplate);
 });
