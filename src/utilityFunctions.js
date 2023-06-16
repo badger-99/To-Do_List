21f1b120dce0c3aba6b55d29f815cf78e7f55e7a
Alfred Peter <alpeter91@gmail.com>
2023-06-16 09:48:14 +0300

    Update UtilityFunctions.js

--- src/utilityFunctions.js
+++ src/utilityFunctions.js
@@ -1,27 +1,27 @@
 class UtilityFunctions {
-  // Saving to Local Storage -> uses bookArray
+  // Saving to Local Storage -> uses taskArray
   static setStorage = (array) => {
     localStorage.setItem('toDo-list', JSON.stringify(array));
   };
 
   // Getting from Local Storage
   static getStorage = () => {
-    const retrievedBooks = JSON.parse(localStorage.getItem('toDo-list'));
-    if (retrievedBooks && Array.isArray(retrievedBooks)) {
-      return retrievedBooks;
+    const retrievedTasks = JSON.parse(localStorage.getItem('toDo-list'));
+    if (retrievedTasks && Array.isArray(retrievedTasks)) {
+      return retrievedTasks;
     }
     return []; // Return an empty array if storage is empty or not an array
   };
 
   // Showing books on browser  -> uses container
   static showTasks = (element, template) => {
-    const retrievedBooks = this.getStorage();
-    if (retrievedBooks.length > 0) {
-      element.innerHTML = retrievedBooks
-        .map((book, index) => template(book, index))
+    const retrievedTasks = this.getStorage();
+    if (retrievedTasks.length > 0) {
+      element.innerHTML = retrievedTasks
+        .map((task, index) => template(task, index))
         .join('');
     } else {
-      element.innerHTML = '<h2>There is nothing to do.</h2>';
+      element.innerHTML = `<h4 id='empty'>there is nothing To Do</h4>`;
     }
   };
 
@@ -30,10 +30,15 @@ class UtilityFunctions {
     this.setStorage(array);
   }
 
-  static removeTask(array, idx) {
+  static addTask = (array, task) => {
+    array.push(task);
+    this.setStorage(this.addIndex(array));
+  };
+
+  static removeTask = (array, idx) => {
     array.splice(idx, 1);
-    this.setStorage(array);
-  }
+    this.setStorage(this.addIndex(array));
+  };
 }
 
 export default UtilityFunctions;
