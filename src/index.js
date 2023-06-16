d9633b4e36ab1da59d0ba1b379b8a2c93d1507c7
Alfred Peter <alpeter91@gmail.com>
2023-06-16 09:52:59 +0300

    Show form on browser

--- src/index.js
+++ src/index.js
@@ -6,39 +6,20 @@ import Task from './task.js';
 import UtilityFunctions from './utilityFunctions.js';
 import './style.css';
 
-function component() {
-  const container = document.getElementById('toDo');
-  const heading = document.createElement('div');
-  const form = document.createElement('form')
-  const entry = document.createElement('input');
-  const submit = document.createElement('button')
-  const list = document.createElement('ul');
-  const clear = document.createElement('button');
+const container = document.getElementById('toDo');
+const list = document.createElement('ul');
+list.setAttribute('id', 'ul');
 
-  // Heading
-  heading.setAttribute('id', 'heading');
-  heading.innerHTML = '<h4>Today\'s To Do</h4><i class="fa-solid fa-arrows-rotate"></i>';
-  container.appendChild(heading);
+container.appendChild(heading);
+container.appendChild(form);
+container.appendChild(list);
 
-  // Task input field
-  form.setAttribute('id', 'form')
-  entry.setAttribute('id', 'input');
-  entry.setAttribute('type', 'text');
-  entry.setAttribute('placeholder', 'Add to your list...');
-  submit.setAttribute('type','button')
-  submit.setAttribute('id', 'submit')
-  submit.innerHTML = `<i class="fa-solid fa-arrow-turn-down fa-rotate-90" id='arrow'></i>`;
-  form.appendChild(entry)
-  form.appendChild(submit)
-  container.appendChild(form);
+const tasklist = new TaskList();
+let taskArray = tasklist.getArray();
+taskArray = [...UtilityFunctions.getStorage()];
+UtilityFunctions.showTasks(list, taskTemplate);
 
-  list.innerHTML = tasks.map((task) => taskTemplate(task)).join('');
-  container.appendChild(list);
-
-  // Clear button
-  clear.innerHTML = 'Clear all completed';
-  clear.setAttribute('id', 'clear');
-  container.appendChild(clear);
+container.appendChild(clear);
 
   return container;
 }
