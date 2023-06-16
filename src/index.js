3502d52f7b5afbea881638e22e63bbf0f13929a1
Alfred Peter <alpeter91@gmail.com>
2023-06-16 09:56:11 +0300

    Add task editing and deletion methods

--- src/index.js
+++ src/index.js
@@ -47,3 +47,147 @@ checkboxes.forEach((checkbox) => {
     }
   });
 });
+
+// move buttons
+// const moveBtns = document.querySelectorAll('.move');
+// moveBtns.forEach((moveBtn) => {
+//   moveBtn.addEventListener('click', (e) => {
+//     const remove = moveBtn.nextElementSibling;
+//     e.preventDefault;
+//     moveBtn.classList.toggle('hidden');
+//     remove.classList.toggle('hidden');
+//   });
+// });
+
+// editing and deleting tasks
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('textBox')) {
+    const textBox = e.target;
+    const moveBtn = textBox.nextElementSibling;
+    const removeBtn = moveBtn.nextElementSibling;
+    const taskNum = textBox.dataset.index;
+    const idx = parseInt(taskNum, 10);
+
+    textBox.readOnly = false; // allowing task to be edited
+    moveBtn.classList.add('hidden');
+    removeBtn.classList.remove('hidden');
+
+    textBox.addEventListener('blur', () => {
+      textBox.readOnly = true;
+
+      // saving edited task
+      taskArray[idx].description = textBox.value;
+      const moddedArray = UtilityFunctions.addIndex(taskArray);
+      UtilityFunctions.setStorage(moddedArray);
+
+      // deleting a task
+      removeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(`remove ${idx}`);
+        UtilityFunctions.removeTask(taskArray, idx);
+        UtilityFunctions.showTasks(list, taskTemplate);
+      });
+    });
+
+    // resetting icons
+    document.body.addEventListener('click', (e) => {
+      if (e.target === document.body) {
+        moveBtn.classList.remove('hidden');
+        removeBtn.classList.add('hidden');
+      }
+    });
+  }
+
+  // if (e.target.classList.contains('remove')) {
+  //   const removeBtn = e.target;
+  //   const taskNum = removeBtn.dataset.index;
+  //   const idx = parseInt(taskNum, 10);
+  //   console.log(`remove ${idx}`)
+  // }
+});
+
+// UtilityFunctions.modifyTask(taskArray, idx, textBox.value);
+// UtilityFunctions.setStorage(taskArray)
+// UtilityFunctions.showTasks(list, taskTemplate);
+
+// remove buttons
+// const removeBtns = document.querySelectorAll('.remove');
+// removeBtns.forEach((removeBtn) => {
+//   removeBtn.addEventListener('click', (e) => {
+//     const taskNum = removeBtn.dataset.index;
+//           const idx = parseInt(taskNum, 10);
+//     console.log(idx);
+//     UtilityFunctions.removeTask(taskArray, idx)
+//     UtilityFunctions.showTasks(list, taskTemplate);
+
+//   })
+// })
+
+// list.addEventListener('change', (e) => {
+//   if (e.target.type === 'checkbox') {
+//     const checkbox = e.target;
+//     const label = checkbox.parentNode
+//     if (checkbox.checked) {
+//       console.log("I'm checked");
+//       label.style.textDecoration = 'line-through'
+//     } else {
+//       label.style.textDecoration = 'none';
+//       console.log("I'm un-checked");
+//     }
+//   }
+// });
+
+// list.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   const icon = e.target
+//   if (icon.classList.contains('move')) {
+//     console.log('move');
+//     move.classList.toggle('hidden');
+//     remove.classList.toggle('hidden');
+//     if (icon.classList.contains('remove')) {
+//       const taskNum = remove.dataset.index;
+//       const idx = parseInt(taskNum, 10);
+//       console.log(idx);
+//       move.classList.toggle('hidden');
+//       remove.classList.toggle('hidden');
+//     }
+//   }
+// });
+
+// list.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   const icon = e.target
+//   if (icon.classList.contains('remove')) {
+//     const taskNum = remove.dataset.index;
+//     const idx = parseInt(taskNum, 10);
+//     console.log(idx);
+//     move.classList.toggle('hidden');
+//     remove.classList.toggle('hidden');
+//   }
+// });
+
+// // Selecting completed tasks
+// const checkbox = document.querySelector('.status');
+// checkbox.addEventListener('change', (e) => {
+//   e.preventDefault();
+//   if (e.target.classList.contains('status')) {
+//     if (checkbox.checked) {
+//       console.log("I'm checked");
+//       label.style.textDecoration = 'line-through'
+//     } else {
+//       label.style.textDecoration = 'none';
+//       console.log("I'm un-checked");
+//     }
+//   }
+// });
+
+// Removing tasks
+// container.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   if (e.target.classList.contains('remove')) {
+
+//   }
+//   const taskNum = remove.dataset.index;
+//   const idx = parseInt(taskNum, 10);
+//   console.log(idx)
+// })
