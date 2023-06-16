45d5ff0a59c40dfa47203fa6cfba545f4b58107c
Alfred Peter <alpeter91@gmail.com>
2023-06-16 10:05:04 +0300

    Fix formatting

--- src/form-top-and-bottom.js
+++ src/form-top-and-bottom.js
@@ -1,12 +1,11 @@
 // Heading
 const heading = document.createElement('div');
 heading.setAttribute('id', 'heading');
-heading.innerHTML =
-  `<h4>Today's To Do</h4><a href='#' id='refresh'><i class="fa-solid fa-arrows-rotate"></i></a>`;
+heading.innerHTML = `<h4>Today's To Do</h4><a href='#' id='refresh'><i class="fa-solid fa-arrows-rotate"></i></a>`;
 
 // Clear button
 const clear = document.createElement('button');
 clear.innerHTML = 'Clear all completed';
 clear.setAttribute('id', 'clear');
 
-export{heading, clear}
+export { heading, clear };
