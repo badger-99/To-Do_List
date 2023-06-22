e1476359f0123bb53552436e3e05ea56a6726f9d
Alfred Peter <alpeter91@gmail.com>
2023-06-22 09:35:08 +0300

    Optimize status-update.js

--- src/status-update.js
+++ src/status-update.js
@@ -5,12 +5,9 @@ export default (checkbox, array) => {
   const taskNum = checkbox.dataset.index;
   const idx = parseInt(taskNum, 10);
   if (checkbox.checked === true) {
-    label.style.textDecoration = 'line-through';
     array[idx].completed = true;
-    UtilityFunctions.setStorage(array);
   } else {
-    label.style.textDecoration = 'none';
     array[idx].completed = false;
-    UtilityFunctions.setStorage(array);
   }
+  UtilityFunctions.setStorage(array);
 };
