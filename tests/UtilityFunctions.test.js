06a560e5463e56dfc0ee204e21edf222c8b47a5d
haben mehar <habenmehari71@gmail.com>
2023-06-22 12:03:44 +0300

    remove task test added

--- tests/UtilityFunctions.test.js
+++ tests/UtilityFunctions.test.js
@@ -6,4 +6,28 @@ describe('addTask', () => {
     UtilityFunctions.addTask(testArray, 'random task')
     expect(testArray.length).toBe(1)
   })
-})
+});
+
+describe('remove task', () => {
+  test('it should remove a task from an array.', () => {
+    const testArray = [
+      {
+        index:1,
+        completed:false,
+        description:'task one'
+      },
+      {
+        index:2,
+        completed:false,
+        description:'task two'
+      },
+      {
+        index:3,
+        completed:false,
+        description:'task three'
+      },
+    ]
+    UtilityFunctions.removeTask(testArray, 2)
+    expect(testArray.length).toBe(2)
+  })
+});
