cd72813ab87f8b2a22ef91d85ff9a63d891a4ca2
Alfred Peter <alpeter91@gmail.com>
2023-06-23 18:18:12 +0300

    Add 'clear all completed' test

--- tests/eventListenerFunctions.test.js
+++ tests/eventListenerFunctions.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import { fireEvent } from '@testing-library/dom';
-import { editAndDeleteTasks, statusUpdate } from '../src/eventListenerFunctions.js';
+import { editAndDeleteTasks, statusUpdate, clearCompleted } from '../src/eventListenerFunctions.js';
 
 describe('testing "edit" from editAndDeleteTasks', () => {
   test('task description changes after user input', () => {
@@ -71,6 +71,7 @@ describe('testing "edit" from editAndDeleteTasks', () => {
     expect(taskArray[0].description).toBe('New Changed Value');
   });
 });
+
 describe('testing status changes,', () => {
   test('should flag task as completed.', () => {
     const taskArray = [
@@ -97,5 +98,70 @@ describe('testing status changes,', () => {
     const mockChange = new Event('change');
     checkbox.dispatchEvent(mockChange);
     expect(taskArray[0].completed).toBe(true);
+  });
+});
+
+describe('Clear all completed tasks', () => {
+  test('Objects with completed:true removed from array', () => {
+    const task1 = {
+      completed: true,
+    };
+    const task2 = {
+      completed: false,
+    };
+    const task3 = {
+      completed: true,
+    };
+    const task4 = {
+      completed: false,
+    };
+    const task5 = {
+      completed: true,
+    };
+
+    let taskArray = [task1, task2, task3, task4, task5];
+    const clear = document.createElement('button');
+    const container = document.createElement('ul');
+
+    const template = (task, index) => {
+      const checkingControl = task.completed ? 'checked' : '';
+
+      return `<li class='task-item'>
+      <input type='checkbox' class='status' ${checkingControl} data-index="${index}">
+      <input type="text" class="textBox" id="todo${index + 1}" value="${
+  task.description
+}" readonly data-index="${index}">
+      <i class='fa-solid fa-ellipsis-vertical icon move'></i>
+      <i class='fa-regular fa-trash-can icon remove hidden' data-index="${index}"></i>
+      </li>`;
+    };
+
+    clear.addEventListener('click', (e) => {
+      e.preventDefault();
+      const moddedArray = [];
+      taskArray = clearCompleted(taskArray, moddedArray, container, template);
+    });
+
+    const mockClick = new Event('click');
+    clear.dispatchEvent(mockClick);
+
+    let cleared;
+    if (taskArray.length === 2) {
+      let statusCheck = 0;
+      taskArray.forEach((task) => {
+        if (task.completed === false) {
+          statusCheck += 1;
+        }
+      });
+      if (statusCheck === 2) {
+        cleared = true;
+      } else {
+        cleared = false;
+      }
+    } else {
+      cleared = false;
+    }
+
+    expect(cleared).toBe(true);
   });
 });
