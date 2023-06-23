5a10950f020191423672c9f9f3f33f65738530dd
Alfred Peter <alpeter91@gmail.com>
2023-06-23 17:14:14 +0300

    Fix 'edit-task' test bug

--- tests/eventListenerFunctions.test.js
+++ tests/eventListenerFunctions.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-
+import { fireEvent } from '@testing-library/dom';
 import { editAndDeleteTasks, statusUpdate, clearCompleted } from '../src/eventListenerFunctions.js';
 
 describe('testing "edit" from editAndDeleteTasks', () => {
@@ -9,22 +9,37 @@ describe('testing "edit" from editAndDeleteTasks', () => {
     const task = {
       description: 'Change this!',
       completed: false,
-      index:1,
-    }
+      index: 1,
+    };
 
-    const taskArray = [task]
-    const container = document.createElement('ul')
-    const li = document.createElement('li')
-    const textBox = document.createElement('input')
-    textBox.type = 'text'
-    textBox.className = 'textBox'
-    textBox.id = 'todo1'
-    textBox.value = 'Change This!'
-    textBox.setAttribute('readonly','readonly')
-    textBox.setAttribute('data-index', '0')
-    
-    li.appendChild(textBox)
-    container.appendChild(li)
+    const taskArray = [task];
+    const container = document.createElement('ul');
+    const li = document.createElement('li');
+
+    const textBox = document.createElement('input');
+    textBox.type = 'text';
+    textBox.className = 'textBox';
+    textBox.id = 'todo1';
+    textBox.value = 'Change This!';
+    textBox.setAttribute('readonly', 'readonly');
+    textBox.setAttribute('data-index', '0');
+
+    const move = document.createElement('i');
+    move.classList.add('fa-solid', 'fa-ellipsis-vertical', 'icon', 'move');
+
+    const remove = document.createElement('i');
+    remove.classList.add(
+      'fa-regular',
+      'fa-trash-can',
+      'icon',
+      'remove',
+      'hidden'
+    );
+
+    li.appendChild(textBox);
+    li.appendChild(move);
+    li.appendChild(remove);
+    container.appendChild(li);
 
     const template = (task, index) => {
       const checkingControl = task.completed ? 'checked' : '';
@@ -40,24 +55,23 @@ describe('testing "edit" from editAndDeleteTasks', () => {
     };
 
     container.addEventListener('click', (e) => {
+      console.log('click heard', e.target);
       if (e.target.classList.contains('textBox')) {
-        const textBox = e.target;
-        editAndDeleteTasks(textBox, taskArray, container, template);
+        const textbox = e.target;
+        console.log('text box found');
+        editAndDeleteTasks(textbox, taskArray, container, template);
+        console.log('changes complete');
       }
+      // console.log('event done');
     });
 
-    const mockClick = new Event('click')
-    const mockBlur = new Event('blur')
+    const mockClick = new Event('click', { bubbles: true });
+    const mockBlur = new Event('blur');
 
-    container.dispatchEvent(mockClick)
-    textBox.value = 'New Changed Value'
-    textBox.dispatchEvent(mockBlur)
-    
+    textBox.dispatchEvent(mockClick);
+    fireEvent.change(textBox, { target: { value: 'New Changed Value' } });
+    textBox.dispatchEvent(mockBlur);
+
     expect(taskArray[0].description).toBe('New Changed Value');
-    
-    // fireEvent.change(textBox, {
-    //   target: { value: 'Another New Changed Value' },
-    // });
-
   })
 })
